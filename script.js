254dc2cff479a7a96fe403909c674dcfcfe09e92
skaz46 <73730853+skaz46@users.noreply.github.com>
2026-07-08 11:58:33 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,50 @@
+const streakElement = document.getElementById("streak");
+const startDateElement = document.getElementById("startDate");
+
+function updateStreak() {
+
+    const startDate = localStorage.getItem("startDate");
+
+    console.log("Stored date:", startDate);
+
+    if (!startDate) {
+        streakElement.innerText = "0 Days";
+        startDateElement.innerText = "Not Set";
+        return;
+    }
+
+    const start = new Date(startDate);
+    const today = new Date();
+
+    const diffTime = today - start;
+    const diffDays = Math.floor(diffTime / (1000 * 60 * 60 * 24));
+
+    streakElement.innerText = diffDays + " Days";
+    startDateElement.innerText = start.toDateString();
+}
+
+document.getElementById("startBtn").addEventListener("click", () => {
+
+    localStorage.setItem(
+        "startDate",
+        new Date().toISOString()
+    );
+
+    updateStreak();
+
+    alert("Journey Started!");
+});
+
+document.getElementById("resetBtn").addEventListener("click", () => {
+
+    localStorage.setItem(
+        "startDate",
+        new Date().toISOString()
+    );
+
+    updateStreak();
+
+    alert("Streak Reset!");
+});
+
+updateStreak();
